e61749e0be01babb1b7760da79a697f5a07b7749
Pomianowski <kontakt@rapiddev.pl>
2021-06-01 00:06:17 +0200

    Add router.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
-import { Toast } from './components/bootstrap.js'
 import Cookie from './components/cookie.js'
+import Router from './components/router.js'
+
+import { Toast } from './components/bootstrap.js'
 
 new Cookie();
+new Router();
+
+//https://getbootstrap.com/docs/5.0/getting-started/javascript/
 Array.from(document.querySelectorAll('.toast')).forEach(toastNode => new Toast(toastNode))
---
+++ public/js/components/router.js
@@ -0,0 +1,13 @@
+export const name = 'Router';
+
+export default class Router {
+
+    constructor(ctx) {
+        this.routes();
+    }
+
+    routes()
+    {
+        console.log('routes');
+    }
+}
