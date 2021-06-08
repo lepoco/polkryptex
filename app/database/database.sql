2f5864e964c965a9e630e3ea615a4a57f2c67534
KujawskiSzymon <KujawskiSzymon0@gmail.com>
2021-06-08 11:52:27 +0200

    database renaming fix

--- app/database/database.sql
+++ app/database/database.sql
@@ -1,5 +1,5 @@
 -- Sessions table
-CREATE TABLE IF NOT EXISTS forward_sessions (
+CREATE TABLE IF NOT EXISTS polkryptex_sessions (
 	session_id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	user_id INT (6),
 	session_key INT (20) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS forward_sessions (
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- Users table
-CREATE TABLE IF NOT EXISTS forward_users (
+CREATE TABLE IF NOT EXISTS polkryptex_users (
 	user_id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	user_name VARCHAR (128) NOT NULL,
 	user_display_name VARCHAR (128),
@@ -21,31 +21,31 @@ CREATE TABLE IF NOT EXISTS forward_users (
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- Global statistics types
-CREATE TABLE IF NOT EXISTS forward_global_statistics_types (
+CREATE TABLE IF NOT EXISTS polkryptex_global_statistics_types (
 	type_id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	type_name VARCHAR (32)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
-INSERT IGNORE INTO forward_global_statistics_types (type_name) VALUES ('unknown') ON DUPLICATE KEY UPDATE type_name=type_name;
-INSERT IGNORE INTO forward_global_statistics_types (type_name) VALUES ('query') ON DUPLICATE KEY UPDATE type_name=type_name;
-INSERT IGNORE INTO forward_global_statistics_types (type_name) VALUES ('page') ON DUPLICATE KEY UPDATE type_name=type_name;
-INSERT IGNORE INTO forward_global_statistics_types (type_name) VALUES ('action') ON DUPLICATE KEY UPDATE type_name=type_name;
+INSERT IGNORE INTO polkryptex_global_statistics_types (type_name) VALUES ('unknown') ON DUPLICATE KEY UPDATE type_name=type_name;
+INSERT IGNORE INTO polkryptex_global_statistics_types (type_name) VALUES ('query') ON DUPLICATE KEY UPDATE type_name=type_name;
+INSERT IGNORE INTO polkryptex_global_statistics_types (type_name) VALUES ('page') ON DUPLICATE KEY UPDATE type_name=type_name;
+INSERT IGNORE INTO polkryptex_global_statistics_types (type_name) VALUES ('action') ON DUPLICATE KEY UPDATE type_name=type_name;
 
 -- Global statistics types
-CREATE TABLE IF NOT EXISTS forward_global_statistics_tags (
+CREATE TABLE IF NOT EXISTS polkryptex_global_statistics_tags (
 	tag_id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	tag_name VARCHAR (32)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
-INSERT IGNORE INTO forward_global_statistics_tags (tag_name) VALUES ('unknown') ON DUPLICATE KEY UPDATE tag_name=tag_name;
+INSERT IGNORE INTO polkryptex_global_statistics_tags (tag_name) VALUES ('unknown') ON DUPLICATE KEY UPDATE tag_name=tag_name;
 
 -- Global statistics
-CREATE TABLE IF NOT EXISTS forward_global_statistics (
+CREATE TABLE IF NOT EXISTS polkryptex_global_statistics (
 	statistic_id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	statistic_type INT (6) UNSIGNED NOT NULL DEFAULT 1,
-	CONSTRAINT fk_statistic_type FOREIGN KEY (statistic_type) REFERENCES forward_global_statistics_types (type_id),
+	CONSTRAINT fk_statistic_type FOREIGN KEY (statistic_type) REFERENCES polkryptex_global_statistics_types (type_id),
 	statistic_tag INT (6) UNSIGNED DEFAULT NULL,
-	CONSTRAINT fk_statistic_tag FOREIGN KEY (statistic_tag) REFERENCES forward_global_statistics_tags (tag_id),
+	CONSTRAINT fk_statistic_tag FOREIGN KEY (statistic_tag) REFERENCES polkryptex_global_statistics_tags (tag_id),
 	statistic_user_id INT (6) UNSIGNED DEFAULT NULL,
-	CONSTRAINT fk_statistic_user_id FOREIGN KEY (statistic_user_id) REFERENCES forward_users (user_id),
+	CONSTRAINT fk_statistic_user_id FOREIGN KEY (statistic_user_id) REFERENCES polkryptex_users (user_id),
 	statistic_user_logged_in BOOLEAN DEFAULT false,
 	statistic_ip VARCHAR (39),
 	statistic_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
