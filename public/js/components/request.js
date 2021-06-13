47d242c1ea9a6e7ecbf1be868af525ce4a420a07
Pomianowski <kontakt@rapiddev.pl>
2021-06-13 17:14:39 +0200

    Add JSON verify

--- public/js/components/request.js
+++ public/js/components/request.js
@@ -1,3 +1,5 @@
+import Toast from './toast.js'
+
 export const name = 'Request';
 
 export default class Request {
@@ -6,44 +8,28 @@ export default class Request {
         document.querySelector(form).addEventListener('submit', event => Request.ajax(event, document.querySelector(form), action));
     }
 
-    static ajax(event, form, callAction)
-    {
+    static ajax(event, form, callAction) {
         event.preventDefault();
 
         const ENDPOINT = app.props.ajax;
-
         let data = new FormData(form);
-        // data.append('user', 'person');
-        // data.append('pwd', 'password');
-        // data.append('organization', 'place');
-        // data.append('requiredkey', 'key');
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', ENDPOINT, true);
         xhr.onload = function () {
-            // do something to response
-            callAction('OK', this.responseText);
+            if (Request.isJson(this.responseText)) {
+                //Action
+                callAction('OK', this.responseText);
+            }
+            else {
+                Toast.send('Error', 'An error occurred while submitting the form.', 'alert');
+            }
         };
         xhr.send(data);
+    }
 
-        // var xmlhttp = new XMLHttpRequest();
-
-        // xmlhttp.onreadystatechange = function () {
-        //     if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-        //         callAction('OK', xmlhttp.responseText);
-        //         // if (xmlhttp.status == 200) {
-        //         //     document.getElementById("myDiv").innerHTML = ;
-        //         // }
-        //         // else if (xmlhttp.status == 400) {
-        //         //     alert('There was an error 400');
-        //         // }
-        //         // else {
-        //         //     alert('something else other than 200 was returned');
-        //         // }
-        //     }
-        // };
-
-        // xmlhttp.open('POST', ENDPOINT, true);
-        //xmlhttp.send();
+    static isJson(string) {
+        if (string == '') { return false; }
+        if (/^[\],:{ }\s]*$/.test(string.replace(/\\["\\\/bfnrtu]/g, "@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) { return true; } else { return false; }
     }
 }
--- public/js/pages/home.js
+++
@@ -1,4 +0,0 @@
-import Toast from './../components/toast.js'
-
-Toast.send('Header', 'Message');
-console.log('123');
