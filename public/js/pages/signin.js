0cb468526f071e69098f7079fe7d75c30a4e89a8
Pomianowski <kontakt@rapiddev.pl>
2021-06-20 15:49:32 +0200

    User cookie lax

--- public/js/pages/signin.js
+++ public/js/pages/signin.js
@@ -5,7 +5,7 @@ import Toast from "./../components/toast.js";
 
 Request.register("#signin", function (status, response) {
   if (response.status === "S01") {
-    Cookies.set('user', response.content.token, { expires: 7, path: '/', secure: true });
+    Cookies.set('user', response.content.token, { expires: 7, path: '/', secure: true, sameSite: 'Lax' });
     window.location.href = app.props.baseUrl + app.props.dashboard;
   } else {
     Toast.send(
