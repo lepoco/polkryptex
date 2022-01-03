deb950d41d921c3b62929b6b455eff8faea93e92
Pomianowski <kontakt@rapiddev.pl>
2022-01-03 13:36:01 +0100

    Fix invalid ts name

--- app/common/views/panel/users-add.blade.php
+++ app/common/views/panel/users-add.blade.php
@@ -11,62 +11,64 @@
     </div>
 
     <div class="col-12">
-    <form id="panelAddUser" method="POST">
-    <input type="hidden" name="action" value="PanelAddUser" />
-    <input type="hidden" name="nonce" value="@nonce('paneladduser')" />
+      <form id="panelAddUser" method="POST">
+        <input type="hidden" name="action" value="PanelAddUser" />
+        <input type="hidden" name="nonce" value="@nonce('paneladduser')" />
 
-    <div class="row">
-      <div class="col-12 -mt-1">
-        <div class="floating-input -reveal">
-          <select class="floating-input__field" placeholder="@translate('Role')"
-            name="role">
-            <option value="default">@translate('Default')</option>
-            <option value="manager">@translate('Manager')</option>
-            <option value="analyst">@translate('Analyst')</option>
-            <option value="admin">@translate('Admin')</option>
-          </select>
-          <label for="role">@translate('Role')</label>
+        <div class="row">
+          <div class="col-12 -mt-1">
+            <div class="floating-input -reveal">
+              <select class="floating-input__field" placeholder="@translate('Role')" name="role">
+                <option value="default">@translate('Default')</option>
+                <option value="manager">@translate('Manager')</option>
+                <option value="analyst">@translate('Analyst')</option>
+                <option value="admin">@translate('Admin')</option>
+              </select>
+              <label for="role">@translate('Role')</label>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="floating-input -reveal">
+              <input class="floating-input__field" type="email" name="user_email" placeholder="@translate('Email')">
+              <label for="user_email">@translate('Email')</label>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="floating-input -reveal">
+              <input class="floating-input__field" type="text" name="user_display_name"
+                placeholder="@translate('Name')">
+              <label for="user_display_name">@translate('Name')</label>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="floating-input -reveal">
+              <input class="floating-input__field" type="password" name="user_password"
+                placeholder="@translate('Password')">
+              <label for="user_password">@translate('Password')</label>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="floating-input -reveal">
+              <input class="floating-input__field" type="password" name="user_password_confirm"
+                placeholder="@translate('Confirm password')">
+              <label for="user_password_confirm">@translate('Confirm password')</label>
+            </div>
+          </div>
+
+          <div class="col-12 -mb-2">
+            <hr>
+          </div>
+
+          <div class="col-12">
+            <button type="submit" class="btn btn-dark btn-mobile -lg-mr-1 -reveal">@translate('Add')</button>
+          </div>
+
         </div>
-      </div>
-
-      <div class="col-12">
-        <div class="floating-input -reveal">
-          <input class="floating-input__field" type="email" name="user_email" placeholder="@translate('Email')">
-          <label for="user_email">@translate('Email')</label>
-        </div>
-      </div>
-      
-      <div class="col-12">
-        <div class="floating-input -reveal">
-          <input class="floating-input__field" type="text" name="user_display_name" placeholder="@translate('Name')">
-          <label for="user_display_name">@translate('Name')</label>
-        </div>
-      </div>
-
-      <div class="col-12">
-        <div class="floating-input -reveal">
-          <input class="floating-input__field" type="password" name="user_password" placeholder="@translate('Password')">
-          <label for="user_password">@translate('Password')</label>
-        </div>
-      </div>
-
-      <div class="col-12">
-        <div class="floating-input -reveal">
-          <input class="floating-input__field" type="password" name="user_password_confirm" placeholder="@translate('Confirm password')">
-          <label for="user_password_confirm">@translate('Confirm password')</label>
-        </div>
-      </div>
-
-      <div class="col-12 -mb-2">
-        <hr>
-      </div>
-      
-      <div class="col-12">
-        <button type="submit" class="btn btn-dark btn-mobile -lg-mr-1 -reveal">@translate('Add')</button>
-      </div>
-
-    </div>
-  </form>
+      </form>
     </div>
   </div>
 </div>
